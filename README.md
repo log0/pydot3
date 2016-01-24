e8a6ed91cab5992f92179a3b70dba29c0e855567
echio <Eric Chio "Log0>
2016-01-23 19:56:53 -0800

    Added other files as needed for a package.

---
+++ README.md
@@ -0,0 +1,26 @@
+pydot3
+======
+
+## Python 2 and Python 3 compatible interface to Graphviz's Dot.
+
+Includes fixes from https://github.com/erocarrera/pydot, which is not compatible with Python 3.x for some reasons.
+
+Installation
+------------
+
+### pip / pip3:
+<pre>
+pip3 install pydot3
+pip install pydot3
+</pre>
+
+License
+=======
+
+This code is distributed under the MIT license.
+
+Prerequisites:
+--------------
+
+- pyparsing
+- GraphViz
---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import sys
+from setuptools import setup
+
+import os
+
+os.environ['COPY_EXTENDED_ATTRIBUTES_DISABLE'] = 'true'
+os.environ['COPYFILE_DISABLE'] = 'true'
+
+version = "1.0"
+
+config = dict(
+    name = 'pydot3',
+    version = version,
+    description = 'Python 2 and Python 3 compatible interface to Graphviz\'s Dot',
+    author = 'Eric Chio',
+    author_email = 'ckieric@gmail.com',
+    url = 'http://www.github.com/log0/pydot3/',
+    download_url = 'https://github.com/log0/pydot3/archive/%s.zip' % version,
+    license = 'MIT',
+    keywords = 'graphviz dot graphs visualization pydot pydot2 pydot3',
+    platforms = ['any'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+    py_modules = ['pydot', 'dot_parser'],
+    install_requires = ['pyparsing', 'setuptools'],
+    data_files = [('.', ['LICENSE', 'README'])])
+
+
+if sys.version_info >= (3,):
+    config.update(dict(
+        use_2to3=True,
+    ))
+
+setup(**config)
