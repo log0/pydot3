d33da8a07bf0999038c6199eedf9135974617791
jnthomas8 <jnthomas8@gmail.com>
2016-02-18 21:45:43 -0500

    Resolved #1

--- pydot/dot_parser.py
+++ pydot/dot_parser.py
@@ -23,7 +23,7 @@ import codecs
 
 from pyparsing import __version__ as pyparsing_version
 
-from pyparsing import ( nestedExpr, Literal, CaselessLiteral, Word, Upcase, OneOrMore, ZeroOrMore,
+from pyparsing import ( nestedExpr, Literal, CaselessLiteral, Word, upcaseTokens, OneOrMore, ZeroOrMore,
     Forward, NotAny, delimitedList, oneOf, Group, Optional, Combine, alphas, nums,
     restOfLine, cStyleComment, nums, alphanums, printables, empty, quotedString,
     ParseException, ParseResults, CharsNotIn, dblQuotedString, QuotedString, ParserElement )
